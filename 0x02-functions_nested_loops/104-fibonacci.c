35de615bdd83f67321e7829657c7c4be669dd390
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2023-12-20 01:05:29 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,81 @@
 #include <stdio.h>
 
 /**
+ * Custom data structure to represent large numbers
+ */
+typedef struct {
+    unsigned int digits[3000];
+    int size;
+} BigNumber;
+
+/**
+ * Initializes a BigNumber with a single digit
+ */
+BigNumber initBigNumber(int digit) {
+    BigNumber result;
+    for (int i = 0; i < 3000; i++) {
+        result.digits[i] = 0;
+    }
+    result.digits[0] = digit;
+    result.size = 1;
+    return result;
+}
+
+/**
+ * Adds two BigNumbers
+ */
+BigNumber addBigNumbers(BigNumber a, BigNumber b) {
+    BigNumber result = initBigNumber(0);
+    int carry = 0;
+
+    for (int i = 0; i < a.size || i < b.size || carry; i++) {
+        int sum = a.digits[i] + b.digits[i] + carry;
+        result.digits[i] = sum % 10;
+        carry = sum / 10;
+        if (i == result.size && (a.digits[i] != 0 || b.digits[i] != 0 || carry != 0)) {
+            result.size++;
+        }
+    }
+
+    return result;
+}
+
+/**
+ * Prints a BigNumber
+ */
+void printBigNumber(BigNumber num) {
+    for (int i = num.size - 1; i >= 0; i--) {
+        printf("%u", num.digits[i]);
+    }
+}
+
+/**
  * print_fibonacci - Prints the first 98 Fibonacci numbers
  */
-void print_fibonacci(void)
-{
-	unsigned long int prev = 1, curr = 2, next, count;
+void print_fibonacci(void) {
+    BigNumber prev = initBigNumber(1);
+    BigNumber curr = initBigNumber(2);
+    BigNumber next;
 
-	printf("%lu, %lu, ", prev, curr);
+    printBigNumber(prev);
+    printf(", ");
+    printBigNumber(curr);
+    printf(", ");
 
-	for (count = 2; count < 98; count++)
-	{
-		next = prev + curr;
+    for (int count = 3; count <= 98; count++) {
+        next = addBigNumbers(prev, curr);
 
-		if (count < 97)
-			printf("%lu, ", next);
-		else
-			printf("%lu\n", next);
+        if (count < 98) {
+            printBigNumber(next);
+            printf(", ");
+        } else {
+            printBigNumber(next);
+            printf("\n");
+        }
 
-		prev = curr;
-		curr = next;
-	}
+        prev = curr;
+        curr = next;
+    }
 }
 
 /**
@@ -28,9 +83,7 @@ void print_fibonacci(void)
  *
  * Return: 0 on success
  */
-int main(void)
-{
-	print_fibonacci();
-
-	return (0);
+int main(void) {
+    print_fibonacci();
+    return (0);
 }
