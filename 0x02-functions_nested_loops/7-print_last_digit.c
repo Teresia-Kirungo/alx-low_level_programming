aebe6f10a2eeb0422cbc0e9c52790c6097e4f509
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2023-12-20 00:55:09 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,27 +9,27 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
 
-    /* Special case for INT_MIN */
-    if (n == INT_MIN)
-    {
-        last_digit = 8;  /* The last digit of INT_MIN is 8 */
-        _putchar('8');
-    }
-    else
-    {
-        /* Get the absolute value of the number */
-        if (n < 0)
-            n = -n;
+	/* Special case for INT_MIN */
+	if (n == INT_MIN)
+	{
+		last_digit = 8;  /* The last digit of INT_MIN is 8 */
+		_putchar('8');
+	}
+	else
+	{
+		/* Get the absolute value of the number */
+		if (n < 0)
+			n = -n;
 
-        /* Extract the last digit */
-        last_digit = n % 10;
+		/* Extract the last digit */
+		last_digit = n % 10;
 
-        /* Print the last digit */
-        _putchar(last_digit + '0');
-    }
+		/* Print the last digit */
+		_putchar(last_digit + '0');
+	}
 
-    /* Return the value of the last digit */
-    return last_digit;
+	/* Return the value of the last digit */
+	return last_digit;
 }
