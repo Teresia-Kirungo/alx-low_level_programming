72e740602fdc18e8e102e05c8dd3d0b1fd5c1c62
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-05 01:13:17 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,21 @@
  */
 char *rot13(char *str)
 {
-    int i, j;
-    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; alphabet[j] != '\0'; j++)
-        {
-            if (str[i] == alphabet[j])
-            {
-                str[i] = rot13[j];
-                break;
-            }
-        }
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
 
-    return (str);
+	return (str);
 }
