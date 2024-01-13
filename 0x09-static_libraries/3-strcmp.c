52a1e1931e0943b6ee44f1ba46fe4a6f340e6d8a
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-13 07:04:04 +0300

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,13 +1,15 @@
+#include "main.h"
+
 /**
  * _strcmp - compares two strings
  * @s1: pointer to the first string
  * @s2: pointer to the second string
  *
- * Return: an integer less than, equal to, or greater than zero if s1 is found
+ * Return: an int lessthan, equal to, or greaterthan zero if s1 is found
  *         to be less than, equal to, or greater than s2, respectively
  */
+
 int _strcmp(char *s1, char *s2)
 {
-    /* Empty function definition */
     return (0);
 }
