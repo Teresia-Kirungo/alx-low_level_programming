a38fa4200dc29f09bdad75829e5d4a1bf8d07312
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-13 06:40:50 +0300

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,12 +1,5 @@
 #include "main.h"
 
-/**
- * _strlen - calculates the length of a string
- * @s: pointer to the string
- *
- * Return: the length of the string
- */
-
 int _strlen(char *s)
 {
 	return (0);
