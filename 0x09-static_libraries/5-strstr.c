c19db1422251321351843f2fc11d26afb5b0114a
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-13 07:13:05 +0300

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -10,9 +10,8 @@
  * Return: a pointer to the beginning of the located substring,
  *         or NULL if the substring is not found
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-    /* Empty function definition */
-    return (NULL);
+	return (NULL);
 }
-
