6ba158f6d98d3a8c3ea89c70d0c327a3a14e3827
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-13 06:31:21 +0300

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -6,11 +6,11 @@
  * @s: pointer to the string to be searched
  * @c: the character to be located
  *
- * Return: a pointer to the first occurrence of the character in the string,
+ * Return: a pointer to the first occurrence of the char in the string,
  *         or NULL if the character is not found
  */
+
 char *_strchr(char *s, char c)
 {
-    /* Empty function definition */
-    return (NULL);
+	return (NULL);
 }
