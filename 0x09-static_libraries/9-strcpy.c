464a1b719312aa5f6d9616a792c85db88cb89a9c
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-13 07:15:47 +0300

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
- * _strcpy - copies the string pointed to by src to the buffer pointed to by dest
+ * _strcpy - copies str pointed to by src to buffer pointed to by dest
  * @dest: pointer to the destination buffer
  * @src: pointer to the source string
  *
  * Return: a pointer to the destination buffer (dest)
  */
+
 char *_strcpy(char *dest, char *src)
 {
-    /* Empty function definition */
-    return (dest);
+	return (dest);
 }
