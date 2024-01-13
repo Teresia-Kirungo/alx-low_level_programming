a980b7c3f189a116b217b342e5497e6ce47637b9
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2024-01-13 06:23:42 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,11 +1,13 @@
+#include "main.h"
+
 /**
  * _atoi - converts a string to an integer
  * @s: pointer to the string to be converted
  *
  * Return: the converted integer
  */
+
 int _atoi(char *s)
 {
-    /* Empty function definition */
-    return (0);
+	return (0);
 }
